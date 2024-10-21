3202754603f8f44f60e77d4c9557e196124f12a6
PoznanaKing <pozsgain@kkszki.hu>
2024-10-21 08:59:18 +0200

    The database and the migration is done

--- Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0df4f60f75a828c47bb76fedceaa03c9f1624d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
